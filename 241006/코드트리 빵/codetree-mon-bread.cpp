309519f18ad1ae8b1b80b486a4f3c8558f9bbe85
JunHyeok <84581825+k1101jh@users.noreply.github.com>
2024-10-06 23:35:44 +0900

    Title: 코드트리 빵, Time: 4ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241006/코드트리 빵/codetree-mon-bread.cpp
+++ 241006/코드트리 빵/codetree-mon-bread.cpp
@@ -11,14 +11,14 @@ int n, m;
 int dy[4] = {-1, 0, 0, 1};
 int dx[4] = {0, -1, 1, 0};
 
+// 사람들이 대기하는 큐
+queue<pair<int, int>> waiting_q;
 vector<vector<int>> board;
-vector<pair<int, int>> stores_vec;
-list<pair<int, int>> basecamp_list;
-
-queue<pair<Consumer, int>> q;
+vector<vector<int>> visited;
 
 struct Consumer {
 public:
+    Consumer() {}
     Consumer(pair<int, int> cur_pos, pair<int, int> dest, int bit)
     : cur_pos(cur_pos), dest(dest), bit(bit)
     {
@@ -28,74 +28,76 @@ public:
     int bit;
 };
 
-int calc_l1_dist(pair<int, int>& p1, pair<int, int>& p2) {
-    return abs(p1.first - p2.first) + abs(p1.second - p2.second);
-}
+// 현 시점에서 가장 가까운 basecamp 찾아내기
+pair<int, int> get_nearest_basecamp(pair<int, int>& pos, int bit) {
+    vector<vector<int>> visited_copy(visited);
+    queue<pair<int, int>> q2;
 
-void set_start_basecamp() {
-    for(int i=0; i < stores_vec.size(); i++) {
-        int best_dist = INT32_MAX;
-        pair<int, int>& store = stores_vec[i];
-        vector<list<pair<int, int>>::iterator> candidate_camps;
-        
-        // 거리가 가장 짧은 basecamp들 찾기
-        for(auto iter=basecamp_list.begin(); iter != basecamp_list.end(); iter++) {
-            // 가게와 basecamp 거리 계산
-            int dist = calc_l1_dist(store, *iter);
+    q2.push(pos);
 
-            // 새로 계산한 거리가 최소거리면 후보군 비운다음 현재 basecamp를 후보군에 추가
-            if(best_dist > dist) {
-                candidate_camps = vector<list<pair<int, int>>::iterator>();
-                candidate_camps.push_back(iter);
-                best_dist = dist;
-            // 새로 계산한 거리와 최소거리가 같으면 현재 basecamp를 후보군에 추가
-            } else if(best_dist == dist) {
-                candidate_camps.push_back(iter);
-            }
-        }
+    while(!q2.empty()) {
+        pair<int, int> cur = q2.front();
+        q2.pop();
 
-        // 거리가 짧은 basecamp 중 행이 작거나, 행이 같으면 열이 작은 캠프 찾기
-
-        // 거리가 짧은 basecamp가 하나만 있는 경우
-        if(candidate_camps.size() == 1) {    
-            q.push({Consumer(*candidate_camps[0], store, (1 << i)), i + 1});
-            basecamp_list.erase(candidate_camps[0]);
-        // 거리가 짧은 basecamp가 여럿 있는 경우
-        } else {
-            list<pair<int, int>>::iterator best_camp = candidate_camps[0];
-            for(int j=1; j < candidate_camps.size(); j++) {
-                if(candidate_camps[j]->first < best_camp->first) {
-                    best_camp = candidate_camps[j];
-                } else if(candidate_camps[j]->first == best_camp->first && candidate_camps[j]->second < best_camp->second) {
-                    best_camp = candidate_camps[j];
+        for(int i=0; i < 4; i++) {
+            pair<int, int> next = {cur.first + dy[i], cur.second + dx[i]};
+            if(next.first >= 0 && next.first < n && next.second >= 0 && next.second < n
+            && (visited_copy[next.first][next.second] & bit) == 0) {
+                // basecamp를 찾아냈으면 반환
+                if(board[next.first][next.second] == 1) {
+                    return next;
                 }
+                visited_copy[next.first][next.second] |= bit;
+                q2.push(next);
             }
-            q.push({Consumer(*best_camp, store, (1 << i)), i + 1});
-            basecamp_list.erase(best_camp);
         }
     }
+
+    return {-1, -1};
 }
 
 int bfs() {
+    queue<pair<Consumer, int>> q;
     // i에 해당하는 사람이 방문하면 (1 << i)를 & 하기
-    vector<vector<int>> visited(n, vector<int>(n, false));
+    visited = vector<vector<int>>(n, vector<int>(n, false));
 
-    int time = 0;
-    int prev_bit = 0;
-    int human_bit = 0;
+    int time = 1;
     int num_complete = 0;
     int complete_bit = 0;
 
-    while(!q.empty()) {
-        Consumer consumer = q.front().first;
-        time = q.front().second;
-        q.pop();
+    int consumer_cnt = 1;
 
-        // 현재 consumer가 처음 등장한 경우면
-        // basecamp에 위치해 있으므로 해당 basecamp를 봉쇄
-        if((human_bit & consumer.bit) ==  0) {
-            visited[consumer.cur_pos.first][consumer.cur_pos.second] = INT32_MAX;
-            human_bit |= consumer.bit;
+    // 맨 처음 사람
+    pair<int, int> dest = waiting_q.front();
+    waiting_q.pop();
+    int cur_bit = (1 << consumer_cnt);
+    pair<int, int> start_camp = get_nearest_basecamp(dest, cur_bit);
+
+    // 이 basecamp를 다른 사람이 사용하지 못하도록 방문처리
+    visited[start_camp.first][start_camp.second] = INT32_MAX;
+    q.push({Consumer(start_camp, dest, cur_bit), 1});
+    consumer_cnt++;
+
+    Consumer consumer;
+
+    while(!q.empty()) {
+        // 3단계 수행
+        // 큐에서 가져오지 말고, 새로 들어온 사람에 대해 bfs 수행
+        if(q.front().second > time && !waiting_q.empty()) {
+            dest = waiting_q.front();
+            waiting_q.pop();
+            
+            cur_bit = (1 << consumer_cnt);
+            start_camp = get_nearest_basecamp(dest, cur_bit);
+            visited[start_camp.first][start_camp.second] = INT32_MAX;
+
+            consumer = Consumer(start_camp, dest, cur_bit);
+            time = consumer_cnt;
+            consumer_cnt++;
+        } else {
+            consumer = q.front().first;
+            time = q.front().second;
+            q.pop();
         }
 
         // 현재 consumer가 이미 편의점에 도착한 상태라면
@@ -135,28 +137,19 @@ int main() {
     cout.tie(NULL);
 
     cin >> n >> m;
-
     board = vector<vector<int>>(n, vector<int>(n, 0));
-    stores_vec = vector<pair<int, int>>(m, {0, 0});
-    basecamp_list = list<pair<int, int>>();
 
     for(int i=0; i < n; i++) {
         for(int j=0; j < n; j++) {
             cin >> board[i][j];
-            if(board[i][j] == 1) {
-                basecamp_list.push_back({i, j});
-            }
         }
     }
 
+    int y, x;
     for(int i=0; i < m; i++) {
-        cin >> stores_vec[i].first >> stores_vec[i].second;
-        stores_vec[i].first--;
-        stores_vec[i].second--;
+        cin >> y >> x;
+        waiting_q.push({y - 1, x - 1});
     }
-
-    // 가장 가까운 베이스캠프 찾기
-    set_start_basecamp();
 
     cout << bfs() << '\n';
 
