1c61a923287131264e035181aab78d6623ac5279
JunHyeok <84581825+k1101jh@users.noreply.github.com>
2024-10-07 03:04:49 +0900

    Title: 코드트리 빵, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241007/코드트리 빵/codetree-mon-bread.cpp
+++ 241007/코드트리 빵/codetree-mon-bread.cpp
@@ -11,125 +11,37 @@ int n, m;
 int dy[4] = {-1, 0, 0, 1};
 int dx[4] = {0, -1, 1, 0};
 
-// 사람들이 대기하는 큐
-queue<pair<int, int>> waiting_q;
 vector<vector<int>> board;
-vector<vector<int>> visited;
+vector<pair<int, int>> stores;
+vector<vector<int>> dists;
+list<pair<int, int>> positions;
+list<pair<int, int>> basecamps;
 
-struct Consumer {
-public:
-    Consumer() {}
-    Consumer(pair<int, int> cur_pos, pair<int, int> dest, int bit)
-    : cur_pos(cur_pos), dest(dest), bit(bit)
-    {
-    }
-    pair<int, int> cur_pos;
-    pair<int, int> dest;
-    int bit;
-};
+void bfs(pair<int, int> pos) {
+    // 편의점 좌표를 받아와서 각 타일까지의 거리 계산
+    
+    queue<pair<pair<int, int>, int>> q;
+    q.push({pos, 0});
 
-// 현 시점에서 가장 가까운 basecamp 찾아내기
-pair<int, int> get_nearest_basecamp(pair<int, int>& pos, int bit) {
-    vector<vector<int>> visited_copy(visited);
-    queue<pair<int, int>> q2;
+    dists = vector<vector<int>>(n, vector<int>(n, INT32_MAX));
+    dists[pos.first][pos.second] = 0;
 
-    q2.push(pos);
-
-    while(!q2.empty()) {
-        pair<int, int> cur = q2.front();
-        q2.pop();
+    while(!q.empty()) {
+        pair<int, int> cur = q.front().first;
+        int dist = q.front().second;
+        q.pop();
 
         for(int i=0; i < 4; i++) {
             pair<int, int> next = {cur.first + dy[i], cur.second + dx[i]};
-            if(next.first >= 0 && next.first < n && next.second >= 0 && next.second < n
-            && (visited_copy[next.first][next.second] & bit) == 0) {
-                // basecamp를 찾아냈으면 반환
-                if(board[next.first][next.second] == 1) {
-                    return next;
-                }
-                visited_copy[next.first][next.second] |= bit;
-                q2.push(next);
-            }
+
+            if(next.first < 0 || next.first >= n || next.second < 0 || next.second >= n) continue;
+            if(board[next.first][next.second] == INT32_MAX) continue;
+            if(dists[next.first][next.second] != INT32_MAX) continue;
+
+            dists[next.first][next.second] = dist + 1;
+            q.push({next, dist + 1});
         }
     }
-
-    return {-1, -1};
-}
-
-int bfs() {
-    queue<pair<Consumer, int>> q;
-    // i에 해당하는 사람이 방문하면 (1 << i)를 & 하기
-    visited = vector<vector<int>>(n, vector<int>(n, false));
-
-    int time = 1;
-    int num_complete = 0;
-    int complete_bit = 0;
-    int consumer_cnt = 1;
-
-    // 맨 처음 사람
-    pair<int, int> dest = waiting_q.front();
-    waiting_q.pop();
-    int cur_bit = (1 << consumer_cnt);
-    pair<int, int> start_camp = get_nearest_basecamp(dest, cur_bit);
-
-    // 이 basecamp를 다른 사람이 사용하지 못하도록 방문처리
-    visited[start_camp.first][start_camp.second] = INT32_MAX;
-    q.push({Consumer(start_camp, dest, cur_bit), 1});
-    consumer_cnt++;
-
-    Consumer consumer;
-
-    // 모든 사람이 목적지에 도착할 때까지 수행
-    while(num_complete < m) {
-        // 3단계 수행
-        // 큐에서 가져오지 말고, 새로 들어온 사람에 대해 bfs 수행
-        // time=n인 사람이 대기중일 때, n-1 번째 사람이 새로 들어와야 함
-        if((q.empty() || time == consumer_cnt + 1) && !waiting_q.empty()) {
-            dest = waiting_q.front();
-            waiting_q.pop();
-
-            cur_bit = (1 << consumer_cnt);
-            start_camp = get_nearest_basecamp(dest, cur_bit);
-            visited[start_camp.first][start_camp.second] = INT32_MAX;
-
-            consumer = Consumer(start_camp, dest, cur_bit);
-            time = consumer_cnt;
-            consumer_cnt++;
-        } else {
-            consumer = q.front().first;
-            time = q.front().second;
-            q.pop();
-        }
-
-        // 현재 consumer가 이미 편의점에 도착한 상태라면
-        // 건너뛰기
-        if((complete_bit & consumer.bit) > 0) {
-            continue;
-        }
-
-        // 목적지에 도착했는지 확인하기
-        if(consumer.cur_pos == consumer.dest) {
-            visited[consumer.cur_pos.first][consumer.cur_pos.second] = INT32_MAX;
-            num_complete++;
-            complete_bit |= consumer.bit;
-            // 모든 사람이 다 목적지에 도착했으면
-            if(num_complete == m) {
-                return time;
-            }
-            continue;
-        }
-
-        for(int i=0; i < 4; i++) {
-            pair<int, int> next = {consumer.cur_pos.first + dy[i], consumer.cur_pos.second + dx[i]};
-            if(next.first >= 0 && next.first < n && next.second >= 0 && next.second < n
-            && (visited[next.first][next.second] & consumer.bit) == 0) {
-                visited[next.first][next.second] |= consumer.bit;
-                q.push({Consumer(next, consumer.dest, consumer.bit), time + 1});
-            }
-        }
-    }
-
-    return time;
 }
 
 int main() {
@@ -139,20 +51,92 @@ int main() {
 
     cin >> n >> m;
     board = vector<vector<int>>(n, vector<int>(n, 0));
+    dists = vector<vector<int>>(n, vector<int>(n, 0));
 
     for(int i=0; i < n; i++) {
         for(int j=0; j < n; j++) {
             cin >> board[i][j];
+            if(board[i][j] == 1) {
+                basecamps.push_back({i, j});
+            }
         }
     }
 
     int y, x;
     for(int i=0; i < m; i++) {
         cin >> y >> x;
-        waiting_q.push({y - 1, x - 1});
+        stores.push_back({y - 1, x - 1});
     }
 
-    cout << bfs() << '\n';
+    int time = 0;
+    int completed_cnt = 0;
+
+    list<pair<pair<int, int>, pair<int, int>>> consumers_list;
+
+    while(completed_cnt < m) {
+        // 1단계: 편의점 방향으로 움직이기
+        // 편의점을 중심으로 bfs를 해서 각 좌표에 편의점까지의 거리를 기록
+        // 거리가 짧아지는 방향으로 이동
+        for(auto& consumer_info : consumers_list) {
+            // cur_pos를 참조로 바꾸면 오류 발생
+            // consumer_info.first = next 부분에서 cur_pos = next가 되는 문제 발생
+            pair<int, int> cur_pos = consumer_info.first;
+            pair<int, int>& dest = consumer_info.second;
+            bfs(dest);
+
+            int min_dist = INT32_MAX;
+            for(int i=0; i < 4; i++) {
+                pair<int, int> next = {cur_pos.first + dy[i], cur_pos.second + dx[i]};
+
+                if(next.first < 0 || next.first >= n || next.second < 0 || next.second >= n) continue;
+                if(board[next.first][next.second] == INT32_MAX) continue;
+            
+                if(dists[next.first][next.second] < min_dist) {
+                    min_dist = dists[next.first][next.second];
+                    consumer_info.first = next;
+                }
+            }
+        }
+
+        // 2단계: 편의점에 도착했는지 확인하기
+        for(auto iter=consumers_list.begin(); iter != consumers_list.end();) {
+            if(iter->first == iter->second) {
+                completed_cnt++;
+                board[iter->first.first][iter->first.second] = INT32_MAX;
+                iter = consumers_list.erase(iter);
+            } else {
+                iter++;
+            }
+        }
+
+        // 3단계: time에 맞춰 새로운 사람 데려오기
+        if(time < m) {
+            pair<int, int>& dest = stores[time];
+            bfs(dest);
+
+            // 가장 가까운 basecamp 찾기
+            int min_dist = INT32_MAX;
+            pair<int, int> closest_basecamp;
+
+            for(int i=n - 1; i >= 0; i--) {
+                for(int j=n - 1; j >= 0; j--) {
+                    if(board[i][j] == 1 && dists[i][j] <= min_dist) {
+                        min_dist = dists[i][j];
+                        closest_basecamp.first = i;
+                        closest_basecamp.second = j;
+                    }
+                }
+            }
+
+            board[closest_basecamp.first][closest_basecamp.second] = INT32_MAX;
+            consumers_list.push_back({closest_basecamp, dest});
+        }
+
+        // 시간 증가
+        time++;
+    }
+
+    cout << time << '\n';
 
     return 0;
 }
