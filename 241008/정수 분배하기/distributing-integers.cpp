24fd19e7ee9b913f91f661c0cdcd6cae0023f44b
JunHyeok <84581825+k1101jh@users.noreply.github.com>
2024-10-08 21:31:25 +0900

    Title: 정수 분배하기, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241008/정수 분배하기/distributing-integers.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int getM(vector<int>& vec, int k) {
+    int num = 0;
+    for(auto& element : vec) {
+        num += element / k;
+    }
+    return num;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> vec(n, 0);
+
+    for(int i=0; i < n; i++) {
+        cin >> vec[i];
+    }
+
+    int left = 0;
+    int right = 100000;
+
+    int answer = 0;
+
+    while(left <= right) {
+        int mid = (left + right) / 2;
+        int cur_m = getM(vec, mid);
+        if(cur_m >= m) {
+            answer = max(answer, mid);
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    cout << answer << '\n';
+
+    return 0;
+}
