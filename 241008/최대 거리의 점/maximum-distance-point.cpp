e52c8e044baac1e9afff11828a139db2eb989901
JunHyeok <84581825+k1101jh@users.noreply.github.com>
2024-10-08 22:50:00 +0900

    Title: 최대 거리의 점, Time: 77ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241008/최대 거리의 점/maximum-distance-point.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+
+bool valid(vector<int>& vec, int num) {
+    int prev = vec[0];
+    int num_objects = 1;
+    for(int i=1; i < n; i++) {
+        if(vec[i] - prev < num) {
+            continue;
+        } else {
+            prev = vec[i];
+            num_objects++;
+        }
+    }
+    return num_objects >= m;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    cin >> n >> m;
+    vector<int> vec(n, 0);
+
+    for(int i=0; i < n; i++) {
+        cin >> vec[i];
+    }
+
+    int left = 0;
+    int right = 1000000000;
+    int ans = 0;
+
+    while(left <= right) {
+        int mid = (left + right) / 2;
+        
+        if(valid(vec, mid)) {
+            ans = max(ans, mid);
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
