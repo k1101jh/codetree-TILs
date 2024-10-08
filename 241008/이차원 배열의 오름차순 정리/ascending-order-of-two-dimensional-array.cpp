dd80476bbc17b247b95642451797059e67f932b7
JunHyeok <84581825+k1101jh@users.noreply.github.com>
2024-10-08 22:41:06 +0900

    Title: 이차원 배열의 오름차순 정리, Time: 34ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241008/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+
+long long n, k;
+
+int GetNumSmaller(long long a) {
+    int num = 0;
+
+    for(int i=1; i <= n; i++) {
+        num += min(n, a / i);
+    }
+
+    return num;
+}
+
+int main() {
+
+    cin >> n >> k;
+
+    long long left = 1;
+    long long right = n * n;
+    long long ans = n * n;
+
+    while(left <= right) {
+        long long mid = (left + right) / 2;
+        if(GetNumSmaller(mid) >= k) {
+            right = mid - 1;
+            ans = min(ans, mid);
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
